5246247290e35f89eb4818ebc71af856ea921d92
saertna <83655354+saertna@users.noreply.github.com>
2023-09-03 21:07:38 +0200

    test for rateLevelOfMaturity

--- tests/majuritycalculation.test.ts
+++ tests/majuritycalculation.test.ts
@@ -1,6 +1,9 @@
 import { describe } from 'node:test';
-import {statusPointsForLevel} from "../src/levels";
-import {countLayer2AndLayer3Characters, rateProgressiveSummarization} from "../src/majuritycalculation";
+import {
+	countLayer2AndLayer3Characters,
+	rateLevelOfMaturity,
+	rateProgressiveSummarization
+} from "../src/majuritycalculation";
 
 describe('rateProgressiveSummarization', () => {
 	it('if nothing higlighted shall return 0', () => {
@@ -30,9 +33,31 @@ describe('rateProgressiveSummarization', () => {
 
 
 describe('countLayer2AndLayer3Characters', () => {
-	it('return the chars for each input.', () => {
-		const actual = countLayer2AndLayer3Characters('# This is an example Text for Test\nThis is Line 1\nThis is Line 2\nThis is Line 3\n#thisIsATag','This is an example Text for Test', '==This is Line 1==', '*is*');
+	it('return the number chars for each layer.', () => {
+		const actual = countLayer2AndLayer3Characters('# This is an example Text for Test\nThis is Line 1\nThis is Line 2\nThis is Line 3\n#thisIsATag','This is an example Text for Test', 'This is Line 1', 'is');
 		const expected = {charCount:44, highlightedCount:14, boldCount:2};
+		expect(actual).toBe(expected);
+	});
+
+});
+
+
+describe('rateLevelOfMaturity', () => {
+	it('if all classes are at 5, result shall be 5', () => {
+		const actual = rateLevelOfMaturity(5,5,5,5,5);
+		const expected = 5;
+		expect(actual).toBe(expected);
+	});
+
+	it('if all classes but ProgSum are at 5, result shall be 5', () => {
+		const actual = rateLevelOfMaturity(5,5,5,5,0);
+		const expected = 5;
+		expect(actual).toBe(expected);
+	});
+
+	it('if all classes but note length are at 5, result shall be 5', () => {
+		const actual = rateLevelOfMaturity(5,0,5,5,5);
+		const expected = 5;
 		expect(actual).toBe(expected);
 	});
 
