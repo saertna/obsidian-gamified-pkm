1e87d569d38a4f3906431d8bbb0d9a447259eeac
saertna <83655354+saertna@users.noreply.github.com>
2023-09-24 21:51:03 +0200

    notice points with random message

--- src/challengeNotificationText.ts
+++ src/challengeNotificationText.ts
@@ -106,6 +106,62 @@ const twoNoteMessages: string[] = [
 	"You're a 2-note dynamo! 💥 [X] points earned. What's your next explosion?"
 ];
 
+const randomPointNotices: string[] = [
+	"Bazinga! You just snagged [X] points!",
+	"Kaboom! [X] points are now in your pocket!",
+	"Woohoo! [X] points earned! Keep it up!",
+	"Points ahoy! [X] more in the bank!",
+	"Score! [X] points added to your total!",
+	"You're on fire! [X] points in the bag!",
+	"Zippity-zap! [X] points for you!",
+	"Champion move! [X] points are yours!",
+	"Bingo! [X] points just for you!",
+	"Shazam! [X] points to the rescue!",
+	"Ka-ching! [X] points earned!",
+	"Superb! [X] more points for you!",
+	"Bravo! [X] points added to your stash!",
+	"Well done! [X] more points in tow!",
+	"Zap! [X] points, just like that!",
+	"A round of applause for [X] points!",
+	"Whoosh! [X] points are yours!",
+	"Zing! [X] points for your efforts!",
+	"Woo! [X] points, way to go!",
+	"Zesty! [X] points in your tally!",
+	"Great job! [X] more points for you!",
+	"Fantastic! [X] points in the bag!",
+	"Zowie! [X] points just for you!",
+	"Hooray! [X] points in your account!",
+	"Nice one! [X] points in the pocket!",
+	"Woot woot! [X] points, nice work!",
+	"Zigzag! [X] points, keep it up!",
+	"Bam! [X] points, well done!",
+	"Cool beans! [X] points for you!",
+	"Zesty! [X] points, keep it up!",
+	"Splendid! [X] points for your efforts!",
+	"Marvelous! [X] points earned!",
+	"Zany! [X] points, well done!",
+	"Exquisite! [X] points in your stash!",
+	"Outstanding! [X] points for you!",
+	"Zesty! [X] points, nice job!",
+	"Magnificent! [X] points earned!",
+	"Zigzag! [X] points in your tally!",
+	"Terrific! [X] points for you!",
+	"Superb! [X] points, well done!",
+	"Zowie! [X] points for your efforts!",
+	"Excellent! [X] points earned!",
+	"Zap! [X] points in your account!",
+	"Awesome! [X] points, way to go!",
+	"Well played! [X] points for you!",
+	"Zesty! [X] points, nice work!",
+	"Impressive! [X] points, keep it up!",
+	"Brilliant! [X] points, well done!",
+	"Zing! [X] points for your efforts!",
+	"Nice move! [X] points earned!",
+	"Zesty! [X] points in the bag!",
+	"Stellar! [X] points just for you!",
+	"Well deserved! [X] points earned!"
+];
+
 
 export function getRandomMessageWeeklyChallenge(points: number): string {
 	const randomIndex = Math.floor(Math.random() * messagesWeeklyChallenge.length);
@@ -117,6 +173,13 @@ export function getRandomMessageWeeklyChallenge(points: number): string {
 export function getRandomMessageTwoNoteChallenge(points: number): string {
 	const randomIndex = Math.floor(Math.random() * twoNoteMessages.length);
 	const message = twoNoteMessages[randomIndex];
+	return message.replace("[X]", points.toString());
+}
+
+
+export function getRandomMessagePoints(points: number): string {
+	const randomIndex = Math.floor(Math.random() * randomPointNotices.length);
+	const message = randomPointNotices[randomIndex];
 	return message.replace("[X]", points.toString());
 }
 
