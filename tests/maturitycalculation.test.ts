62b407d87aa6fc407b923f73e9fd27ac4de71db7
saertna <83655354+saertna@users.noreply.github.com>
2023-12-02 18:57:36 +0100

    Unit Test 'rateLevelOfMaturity'

--- tests/maturitycalculation.test.ts
+++ tests/maturitycalculation.test.ts
@@ -26,10 +26,90 @@ describe('rateProgressiveSummarization', () => {
 	});
 });
 
+describe('countLayer2AndLayer3Characters', () => {
+	test('count how many summarization parts there are', () => {
+		const content= "---\n" +
+			"initDate: 02.12.23\n" +
+			"title-class: 0➡️\n" +
+			"note-length-class: 0➡️\n" +
+			"inlink-class: 0➡️\n" +
+			"outlink-class: 0➡️\n" +
+			"progressive-summarization-maturity: 4➡️\n" +
+			"note-maturity: 1➡️\n" +
+			"---\n" +
+			"#inbox\n" +
+			"\n" +
+			"\n" +
+			"# Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt\n" +
+			"\n" +
+			"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.   \n" +
+			"\n" +
+			"==Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et **accumsan et iusto odio** dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.==   \n" +
+			"\n" +
+			"Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.   \n" +
+			"\n" +
+			"Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.   \n" +
+			"\n" +
+			"Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis.   \n" +
+			"\n" +
+			"==At vero **eos** et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, **consetetur** sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.== At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, At accusam aliquyam diam diam dolore dolores duo eirmod eos erat, et nonumy sed tempor et et invidunt justo labore Stet clita ea et gubergren, kasd magna no rebum. sanctus sea sed takimata ut vero voluptua. est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur"
+		const filename = "progressive summarization"
+		const layer2 = "=="
+		const layer3 = "**"
+		const result = countLayer2AndLayer3Characters(content, filename, layer2, layer3);
+		const expectedResult = {"boldCount": 35, "charCount": 3124, "highlightedCount": 722}
+
+		expect(result).toEqual(expectedResult);
+	});
+});
 
 
+describe('rateLevelOfMaturity', () => {
+	test('maturity 0', () => {
+		const result= rateLevelOfMaturity(0,0,0,0,0);
+		const expectedResult = 0;
+		expect(result).toEqual(expectedResult);
+	});
+	test('maturity 1', () => {
+		const result= rateLevelOfMaturity(1,2,1,1,0);
+		const expectedResult = 1;
+		expect(result).toEqual(expectedResult);
+	});
+	test('maturity 2', () => {
+		const result= rateLevelOfMaturity(2,2,1,1,0);
+		const expectedResult = 2;
+		expect(result).toEqual(expectedResult);
+	});
+	test('maturity 3', () => {
+		const result= rateLevelOfMaturity(4,3,3,1,0);
+		const expectedResult = 3;
+		expect(result).toEqual(expectedResult);
+	});
+	test('maturity 4', () => {
+		const result= rateLevelOfMaturity(5,5,4,2,0);
+		const expectedResult = 4;
+		expect(result).toEqual(expectedResult);
+	});
+	test('maturity 5', () => {
+		const result= rateLevelOfMaturity(5,5,4,4,0);
+		const expectedResult = 5;
+		expect(result).toEqual(expectedResult);
+	});
 
+	// and prog sum in add (is this possible?
+	test('maturity 1', () => {
+		const result= rateLevelOfMaturity(1,2,1,1,1);
+		const expectedResult = 1;
+		expect(result).toEqual(expectedResult);
+	});
 
+	// prog sum
+	test('maturity 1 with prog. sum. 1', () => {
+		const result= rateLevelOfMaturity(0,2,1,1,1);
+		const expectedResult = 1;
+		expect(result).toEqual(expectedResult);
+	});
+});
 
 jest.mock('../src/maturitycalculation.ts', () => ({
 	...jest.requireActual('../src/maturitycalculation.ts'),
