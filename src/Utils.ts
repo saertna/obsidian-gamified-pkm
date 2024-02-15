fe0a6ed5f1e7f3b1b87b6e4026199893bbfc4e03
saertna <83655354+saertna@users.noreply.github.com>
2024-02-15 21:50:25 +0100

    Refactoring

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,6 +1,7 @@
 import {App, Notice, request} from 'obsidian';
 import {PLUGIN_VERSION} from "./main"
 import type {Moment} from "moment/moment";
+import {boosterRecipes} from "./constants";
 
 //const PLUGIN_VERSION = this.manifest.version;
 //declare let PLUGIN_VERSION:string;
@@ -72,4 +73,75 @@ export function hoursUntilMinutesPassed(inputDate: Moment, minutesToPass: number
 export function isMinutesPassed(inputDate: Moment, minutesPassed: number): boolean {
 	const minutesAgo = window.moment().subtract(minutesPassed, 'minutes'); // Calculate time 'minutesPassed' minutes ago
 	return inputDate.isSameOrBefore(minutesAgo);
+}
+
+export function concatenateStrings(arr: string[]): string {
+	if (arr.length === 1) {
+		return arr[0];
+	} else {
+		const frequencyMap: Record<string, number> = {};
+
+		arr.forEach(item => {
+			if (frequencyMap[item]) {
+				frequencyMap[item]++;
+			} else {
+				frequencyMap[item] = 1;
+			}
+		});
+
+		const resultArray: string[] = [];
+
+		for (const [key, value] of Object.entries(frequencyMap)) {
+			if (value === 1) {
+				resultArray.push(key);
+			} else {
+				resultArray.push(`${value} x ${key}`);
+			}
+		}
+
+		return resultArray.join(', ');
+	}
+}
+
+export function isSameDay(inputDate: Moment): boolean {
+	const currentDate = window.moment(); // Get the current date
+	return currentDate.isSame(inputDate, 'day'); // Check if they are the same day
+}
+
+export function isOneDayBefore(inputDate: Moment): boolean {
+	const oneDayBeforeCurrent = window.moment().subtract(1, 'day'); // Calculate one day before current date
+	return inputDate.isSame(oneDayBeforeCurrent, 'day');
+}
+
+export function rateDirectionForStatusPoints(ratingCurrent: string, ratingNew: number): number {
+	let ratingFaktor: number
+	if (parseInt(ratingCurrent, 10) < ratingNew) {
+		ratingFaktor = ratingNew - parseInt(ratingCurrent, 10)
+	} else {
+		ratingFaktor = 0
+	}
+
+	return ratingFaktor
+}
+
+export function parseBadgeCSV2Dict(csvString: string): Record<string, { date: string, level: string }> {
+	const badgeDict: Record<string, { date: string, level: string }> = {};
+	const rows = csvString.split('##');
+	for (const row of rows) {
+		const [badgeName, dateReceived, level] = row.split(',');
+
+		if (badgeName && dateReceived && level) {
+			badgeDict[badgeName] = {date: dateReceived, level: level};
+		}
+	}
+	return badgeDict;
+}
+
+export function getBoosterRunTimeFromVarName(boosterVarName: string) {
+	for (const element of boosterRecipes) {
+		if (element.varname === boosterVarName) {
+			return element.boosterRunTime as number;
+		}
+	}
+	return 0; // Return null if no matching element is found
 }
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import {defaultSettings, GamificationPluginSettings, ISettings} from './settings
 import format from 'date-fns/format';
 import {
 	avatarInitContent,
-	boosterRecipes,
 	chanceToEarnIngredient,
 	debugLogs,
 	elements,
@@ -33,7 +32,6 @@ import {
 } from './maturitycalculation'
 import {Badge, checkIfReceiveABadge, getBadge, getBadgeDetails, getBadgeForInitLevel, getBadgeForLevel} from './badges'
 import {getLevelForPoints, statusPointsForLevel} from './levels'
-import type {Moment} from 'moment';
 import {
 	getRandomMessagePoints,
 	getRandomMessageTwoNoteChallenge,
@@ -42,7 +40,16 @@ import {
 import {ModalInformationbox} from 'ModalInformationbox';
 import {ModalBooster} from 'ModalBooster';
 import {decryptBoolean, decryptNumber, decryptString, encryptBoolean, encryptNumber, encryptString} from 'encryption';
-import {checkGamifiedPkmVersion, isMinutesPassed} from './Utils'
+import {
+	checkGamifiedPkmVersion,
+	concatenateStrings,
+	getBoosterRunTimeFromVarName,
+	isMinutesPassed,
+	isOneDayBefore,
+	isSameDay,
+	parseBadgeCSV2Dict,
+	rateDirectionForStatusPoints
+} from './Utils'
 import {GamificationMediator} from './GamificationMediator';
 
 let pointsToReceived = 0;
@@ -1476,72 +1483,6 @@ export default class gamification extends Plugin implements GamificationMediator
 }
 
 
-
-
-
-// Example usage
-//const originalData = '2023-08-15 20:00:00';
-//const encryptedData = encryptString(originalData);
-
-// Save `encryptedData` in your settings
-
-// Later, when you retrieve the data
-//const decryptedData = decryptSrting(encryptedData);
-
-// Use `decryptedData` in your plugin
-
-
-function concatenateStrings(arr: string[]): string {
-    if (arr.length === 1) {
-        return arr[0];
-    } else {
-        const frequencyMap: Record<string, number> = {};
-
-        arr.forEach(item => {
-            if (frequencyMap[item]) {
-                frequencyMap[item]++;
-            } else {
-                frequencyMap[item] = 1;
-            }
-        });
-
-        const resultArray: string[] = [];
-
-        for (const [key, value] of Object.entries(frequencyMap)) {
-            if (value === 1) {
-                resultArray.push(key);
-            } else {
-                resultArray.push(`${value} x ${key}`);
-            }
-        }
-
-        return resultArray.join(', ');
-    }
-}
-
-
-function getBoosterRunTimeFromVarName(boosterVarName: string) {
-	for (const element of boosterRecipes) {
-		if (element.varname === boosterVarName) {
-			return element.boosterRunTime as number;
-		}
-	}
-	return 0; // Return null if no matching element is found
-}
-
-
-function isSameDay(inputDate: Moment): boolean {
-	const currentDate = window.moment(); // Get the current date
-	return currentDate.isSame(inputDate, 'day'); // Check if they are the same day
-}
-
-
-function isOneDayBefore(inputDate: Moment): boolean {
-	const oneDayBeforeCurrent = window.moment().subtract(1, 'day'); // Calculate one day before current date
-	return inputDate.isSame(oneDayBeforeCurrent, 'day');
-}
-
-
 async function createAvatarFile(app: App, fileName: string) {
 
 	const existingFile = this.app.vault.getAbstractFileByPath(`${fileName}.md`);
@@ -1580,27 +1521,3 @@ async function replaceFormatStrings(layer2: string, layer3: string) {
 }
 
 
-function rateDirectionForStatusPoints(ratingCurrent: string, ratingNew: number): number {
-	let ratingFaktor: number
-	if (parseInt(ratingCurrent, 10) < ratingNew){
-		ratingFaktor = ratingNew - parseInt(ratingCurrent, 10)
-	} else {
-		ratingFaktor = 0
-	}
-
-	return ratingFaktor
-}
-
-
-function parseBadgeCSV2Dict(csvString: string): Record<string, { date: string, level: string }> {
-	const badgeDict: Record<string, { date: string, level: string }> = {};
-    const rows = csvString.split('##');
-	for (const row of rows) {
-		const [badgeName, dateReceived, level] = row.split(',');
-
-        if (badgeName && dateReceived && level) {
-			badgeDict[badgeName] = { date: dateReceived, level: level };
-        }
-    }
-    return badgeDict;
-}
