75bdfe6e7959d2522a6e7e45b299b932f37a75df
saertna <83655354+saertna@users.noreply.github.com>
2024-03-23 23:22:33 +0100

    Avatar Picture in Profile

---
+++ src/avatar/Fab.svelte
@@ -0,0 +1,12 @@
+<button class="fab" {...$$props} on:click>
+	<slot />
+</button>
+
+<style>
+	.fab {
+		position: absolute;
+		bottom: 1em;
+		right: 1em;
+		padding: var(--size-2-2) var(--size-2-3);
+	}
+</style>
