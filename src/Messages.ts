e54a5071c07f94b294748f58e55ff8af019b3dba
saertna <83655354+saertna@users.noreply.github.com>
2025-04-23 21:56:41 +0200

    Release video for 0.0.93

--- src/Messages.ts
+++ src/Messages.ts
@@ -22,7 +22,7 @@ Special shout out for lulunac27a, who stayed tuned and asked to enable the new b
 
 <div class="gPKM-videoWrapper">
   <div>
-    <iframe width="560" height="315" src="https://youtu.be/SK7pYCiXTFs?si=xujLlJ8BuYjY3Gja" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/SK7pYCiXTFs?si=xAwKfg_x0IR_w_Sr" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
   </div>
 </div>
 
