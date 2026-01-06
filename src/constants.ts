6ca1e3ed76645bbb14fa719d399910d1ac6e4ff6
saertna <83655354+saertna@users.noreply.github.com>
2026-01-06 18:28:20 +0100

    TODO remember

--- src/constants.ts
+++ src/constants.ts
@@ -214,6 +214,7 @@ export const elements = [
 	{ shortName: 'S6', name: 'Amplification Crystal', varName: 'amplificationCrystal', level: 6 }
 ];
 
+//TODO: rewirte all usages pointing to here to data/boosterDefinctions.ts instead.
 export const boosterRecipes = [
   { boosterUseCountName: 'boosterUseCountTemporalTweaker', boosterRunTime: 0, boosterCooldown: 0, boosterDate: 'boosterDateTemporalTweaker', boosterSwitch: '', varname: 'temporalTweaker' ,name: 'Temporal Tweaker', incredients: ['20xS1', '10xS6'], color: '#4CAF50', svg:`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
   <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/>
