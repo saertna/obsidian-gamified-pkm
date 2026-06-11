a54c2d15de03e866799ce3cc8dec334018091c23
saertna <83655354+saertna@users.noreply.github.com>
2026-06-11 20:51:01 +0200

    Resolve linter findings

--- src/GamificationMediator.ts
+++ src/GamificationMediator.ts
@@ -8,7 +8,7 @@ export interface GamificationMediator {
 	setSettingNumber(key: string, value: number): void;
 	setSettingBoolean(key: string, value: boolean): void;
 
-	acquireIngredients(chance:number, min:number, max:number): void;
+	acquireIngredients(chance:number, min:number, max:number): Promise<void> | void;
 
 	saveSettings(): Promise<void>;
 
