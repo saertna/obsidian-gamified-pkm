dd2b19bae0ea9d0763e8bfc40691674ab05f9dcc
saertna <83655354+saertna@users.noreply.github.com>
2026-06-27 08:58:27 +0200

    Prepare Release 0.0.105

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,5 +1,5 @@
 import { Badge } from '../badges'
-export const PLUGIN_VERSION = '0.0.104';
+export const PLUGIN_VERSION = '0.0.105';
 export const pointsNoteMajurity = 100;
 export const pointsMajurity = 10;
 export const pointsForDailyChallenge = 500;
